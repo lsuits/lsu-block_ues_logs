7eac058273e287e65a8753be418fca4d9b8ca69a
Philip Cali <philip.cali@gmail.com>
2011-10-31 14:58:44 -0500

    Special query for pulling the logs.

--- classes/lib.php
+++ classes/lib.php
@@ -18,6 +18,35 @@ class cps_log extends cps_external implements cps_log_types {
         return self::make(self::DROP, $cps_user);
     }
 
+    public static function get_by_special($params, $order = 'timestamp DESC') {
+        global $DB;
+
+        $to_flatten = function($key, $value) {
+            $safe_value = addslashes($value);
+
+            if (preg_match('/name$/', $key)) {
+                $filter = " LIKE '$safe_value%'";
+            } else {
+                $filter = " = '$safe_value'";
+            }
+
+            return "$key $filter";
+        };
+
+        $f = array_map($to_flatten, array_keys($params), array_values($params));
+
+        $where = (!empty($f) ? ' AND ' : '') . implode(' AND ', $f);
+
+        $sql = "SELECT l.userid, u.firstname, u.lastname, l.action, l.timestamp
+            FROM {enrol_cps_logs} l,
+                 {user} u
+                 WHERE u.id = l.userid $where ORDER BY $order";
+
+        $upgraded = function($log) { return cps_log::upgrade($log); };
+
+        return array_map($upgraded, $DB->get_records_sql($sql));
+    }
+
     private static function make($type, $cps_user) {
         $log = new cps_log();
         $log->action = $type;
