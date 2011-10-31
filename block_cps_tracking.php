e7a98a7a857e9912b93a22d19e6c1c915e17bc45
Philip Cali <philip.cali@gmail.com>
2011-10-31 13:18:58 -0500

    Block link

--- block_cps_tracking.php
+++ block_cps_tracking.php
@@ -9,14 +9,38 @@ class block_cps_tracking extends block_list {
         return array('site' => false, 'course' => true, 'my' => false);
     }
 
-    function content() {
+    function get_content() {
         if ($this->content !== null) {
             return $this->content;
         }
 
-        $content - new stdClass;
-        $content->items = array();
-        $content->icons = array();
+        global $CFG, $COURSE, $OUTPUT;
+
+        $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
+        if (!has_capability('moodle/grade:edit', $context)) {
+            return $this->content;
+        }
+
+        require_once $CFG->dirroot . '/blocks/cps_tracking/classes/lib.php';
+
+        $sections = cps_section::from_course($COURSE);
+
+        $by_params = function ($sections) {
+            return array('sectionid' => current($sections)->id);
+        };
+
+        // No sections or ones with enrollment info
+        if (empty($sections) or !cps_log::count($by_params($sections))) {
+            return $this->content;
+        }
+
+        $course_params = array('id' => $COURSE->id);
+        $url = new moodle_url('/blocks/cps_tracking/view.php', $course_params);
+        $link = html_writer::link($url, $this->title);
+
+        $content = new stdClass;
+        $content->items = array($link);
+        $content->icons = array($OUTPUT->pix_icon('i/users', $this->title));
 
         $this->content = $content;
 
