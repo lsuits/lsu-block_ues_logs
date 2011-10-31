753945a9bbf5c5520f8edadae26583c664cb3c96
Philip Cali <philip.cali@gmail.com>
2011-10-31 11:28:22 -0500

    Logging enrollments

--- classes/lib.php
+++ classes/lib.php
@@ -3,6 +3,28 @@
 require_once $CFG->dirroot . '/enrol/cps/publiclib.php';
 cps::require_daos();
 
-class cps_log extends cps_external {
-    // TODO: fill appropriate methods
+interface cps_log_types {
+    const ADD = 'AD';
+    const DROP = 'DR';
+}
+
+class cps_log extends cps_external implements cps_log_types {
+
+    public static function add($cps_user) {
+        return self::make(self::ADD, $cps_user);
+    }
+
+    public static function drop($cps_user) {
+        return self::make(self::DROP, $cps_user);
+    }
+
+    private static function make($type, $cps_user) {
+        $log = new cps_log();
+        $log->action = $type;
+        $log->timestamp = time();
+        $log->userid = $cps_user->userid;
+        $log->sectionid = $cps_user->sectionid;
+
+        return $log;
+    }
 }
--- eventslib.php
+++ eventslib.php
@@ -4,10 +4,26 @@ require_once $CFG->dirroot . '/blocks/cps_tracking/classes/lib.php';
 
 abstract class cps_tracking_event_handler {
     public static function cps_student_process($cps_student) {
-        return true;
+        try {
+            $log = cps_log::add($cps_student);
+
+            $log->save();
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 
     public static function cps_student_release($cps_student) {
-        return true;
+        try {
+            $log = cps_log::drop($cps_student);
+
+            $log->save();
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
