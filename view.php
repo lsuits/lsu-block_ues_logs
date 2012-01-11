c278c9685269aa378714342bbce5bc51addbbe25
Philip Cali <philip.cali@gmail.com>
2012-01-11 15:31:28 -0600

    Added filters

--- view.php
+++ view.php
@@ -6,7 +6,16 @@ require_once 'classes/lib.php';
 require_login();
 
 $courseid = required_param('id', PARAM_INT);
+
+// Filters
 $sectionid = optional_param('sectionid', null, PARAM_INT);
+$action = optional_param('action', null, PARAM_TEXT);
+$ln = optional_param('ln', null, PARAM_TEXT);
+$fn = optional_param('fn', null, PARAM_TEXT);
+
+// Sorts
+$order = optional_param('order', null, PARAM_TEXT);
+$by = optional_param('dir', 'DESC', PARAM_TEXT);
 
 $course_params = array('id' => $courseid);
 
@@ -45,29 +54,50 @@ $to_option = function ($section) use ($course_label) {
     return $course_label($course, $section);
 };
 
-//TODO: print out section drop down; filter by capability
 $section_selector = array_map($to_option, $sections);
 
-$to_tables = function ($in, $section) use ($_s, $course_label, $OUTPUT) {
+$baseurl = new moodle_url('view.php', array(
+    'id' => $courseid,
+    'sectionid' => $sectionid,
+    'action' => $action,
+    'ln' => $ln,
+    'fn' => $fn,
+    'order' => $order,
+    'dir' => $by
+));
+
+$nothing = array('' => $_s('section'));
+echo $OUTPUT->single_select($baseurl, 'sectionid', $section_selector, $sectionid, $nothing);
+
+$nothing = array('' => $_s('action'));
+$actions = array(ues_log::ADD => $_s('add'), ues_log::DROP => $_s('drop'));
+
+echo $OUTPUT->single_select($baseurl, 'action', $actions, $action, $nothing);
+
+echo initial_bar($baseurl->params(), $ln);
+
+$to_tables = function ($in, $section) use ($_s, $course_label, $OUTPUT, $baseurl) {
+    $url_params = $baseurl->params();
+
     $by_params = array('l.sectionid' => $section->id);
 
-    $action_filter = optional_param('action', null, PARAM_TEXT);
+    $action_filter = $url_params['action'];
     if ($action_filter) {
         $by_params['l.action'] = $action_filter;
     }
 
-    $firstname_filter = optional_param('fn', null, PARAM_TEXT);
+    $firstname_filter = $url_params['fn'];
     if ($firstname_filter) {
         $by_params['u.firstname'] = $firstname_filter;
     }
 
-    $lastname_filter = optional_param('ln', null, PARAM_TEXT);
+    $lastname_filter = $url_params['ln'];
     if ($lastname_filter) {
         $by_params['u.lastname'] = $lastname_filter;
     }
 
-    $order = optional_param('order', null, PARAM_TEXT);
-    $by = optional_param('dir', 'DESC', PARAM_TEXT);
+    $order = $url_params['order'];
+    $by = $url_params['dir'];
     if ($order) {
         $order_by = "$order $by";
     } else {
@@ -112,4 +142,45 @@ if (!$success) {
     $OUTPUT->notification($_s('no_logs'));
 }
 
+echo initial_bar($baseurl->params(), $ln);
+
 echo $OUTPUT->footer();
+
+function initial_bar($params, $chosen) {
+    $strall = get_string('all');
+
+    $html = '<div class="initialbar lastinitial">'. get_string('lastname'). ' : ';
+
+    $make_link = function ($value, $letter=null) use ($params) {
+        if (is_null($letter)) {
+            $letter = $value;
+        }
+
+        $params['ln'] = $letter;
+
+        return html_writer::link(new moodle_url('view.php', $params), $value);
+    };
+
+    $bold = function ($text) {
+        return '<strong>' . $text . '</strong>';
+    };
+
+    if (!empty($chosen)) {
+        $html .= $make_link($strall, '');
+    } else {
+        $html .= $bold($strall);
+    }
+
+    $alpha = explode(',', get_string('alphabet', 'langconfig'));
+    foreach ($alpha as $letter) {
+        if ($letter == $chosen) {
+            $html .= ' ' . $bold($letter);
+        } else {
+            $html .= ' ' . $make_link($letter);
+        }
+    }
+
+    $html .= '</div>';
+
+    return $html;
+}
