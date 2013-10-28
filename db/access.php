7281c02a03038ba8800e4103189293d544c21d67
Robert Russo <rrusso@lsu.edu>
2013-10-28 10:22:08 -0500

    access mod

--- db/access.php
+++ db/access.php
@@ -1,11 +1,10 @@
 <?php
 
 $capabilities = array(
-
     'block/ues_logs:addinstance' => array(
         'riskbitmask' => RISK_PERSONAL,
         'captype' => 'read',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'manager' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
