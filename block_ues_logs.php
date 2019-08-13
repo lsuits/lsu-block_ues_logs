b1f093ecf8f69ae9e26323017d2510197c428e38
Robert Russo <rrusso@lsu.edu>
2019-08-13 15:54:50 -0500

    Update to not throw error in 3.7.

--- block_ues_logs.php
+++ block_ues_logs.php
@@ -30,7 +30,7 @@ class block_ues_logs extends block_list {
         return array('site' => false, 'course' => true, 'my' => false);
     }
 
-    function get_content() {
+    public function get_content() {
         if ($this->content !== null) {
             return $this->content;
         }
@@ -43,6 +43,8 @@ class block_ues_logs extends block_list {
         }
 
         require_once $CFG->dirroot . '/blocks/ues_logs/classes/lib.php';
+        require_once($CFG->dirroot . '/enrol/ues/publiclib.php');
+        ues::require_daos();
 
         $sections = ues_section::from_course($COURSE);
 
@@ -55,16 +57,52 @@ class block_ues_logs extends block_list {
             return $this->content;
         }
 
-        $course_params = array('id' => $COURSE->id);
-        $url = new moodle_url('/blocks/ues_logs/view.php', $course_params);
-        $link = html_writer::link($url, $this->title);
-
         $content = new stdClass;
-        $content->items = array($link);
-        $content->icons = array($OUTPUT->pix_icon('i/users', $this->title));
-
+        $content->icons = array();
+        $content->footer = '';
         $this->content = $content;
 
+        // Actually build the content and add it.
+        $this->add_item_to_content([
+            'lang_key' => $this->title,
+            'icon_key' => 'i/users',
+            'page' => 'view',
+            'query_string' => ['id' => $COURSE->id]
+        ]);
+
         return $this->content;
+    }
+
+    /**
+     * Builds and adds an item to the content container for the given params
+     *
+     * @param  array $params  [lang_key, icon_key, page, query_string]
+     * @return void
+     */
+    private function add_item_to_content($params) {
+        if ( ! array_key_exists('query_string', $params)) {
+            $params['query_string'] = [];
+        }
+        $item = $this->build_item($params);
+        $this->content->items[] = $item;
+    }
+
+    /**
+     * Builds a content item (link) for the given params
+     *
+     * @param  array $params  [lang_key, icon_key, page, query_string]
+     * @return string
+     */
+    private function build_item($params) {
+        global $OUTPUT;
+
+        $label = $params['lang_key'];
+
+        $icon = $OUTPUT->pix_icon($params['icon_key'], $label, 'moodle', ['class' => 'icon']);
+
+        return html_writer::link(
+            new moodle_url('/blocks/ues_logs/' . $params['page'] . '.php', $params['query_string']),
+            $icon . $label
+        );
     }
 }
